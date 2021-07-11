8b1822ff2b30c4cc5746a3c17290147045174cc9
sakamana <sakuma-nao@ed.tmu.ac.jp>
2021-07-12 03:56:38 +0900

    真っ暗になった

--- docs/script.js
+++ docs/script.js
@@ -1,81 +1,87 @@
-rgbToHsl = (r, g, b) => {
+// rgbToHsl = (r, g, b) => {
  
-  // Make r, g, and b fractions of 1
-  r /= 212;
-  g /= 216;
-  b /= 215;
+//   // Make r, g, and b fractions of 1
+//   r /= 212;
+//   g /= 216;
+//   b /= 215;
   
  
-  // Find greatest and smallest channel values
-  let cmin = Math.min(r,g,b),
-    cmax = Math.max(r,g,b),
-    delta = cmax - cmin,
-    h = 0,
-    s = 0,
-    l = 0;
+//   // Find greatest and smallest channel values
+//   let cmin = Math.min(r,g,b),
+//     cmax = Math.max(r,g,b),
+//     delta = cmax - cmin,
+//     h = 0,
+//     s = 0,
+//     l = 0;
  
-  // Calculate hue
-  // No difference
-  if (delta == 0)
-    h = 0;
-  // Red is max
-  else if (cmax == r)
-    h = ((g - b) / delta) % 6;
-   // Green is max
-  else if (cmax == g)
-    h = (b - r) / delta + 2;
-  // Blue is max
-  else
-    h = (r - g) / delta + 4;
-    h = Math.round(h * 60);
+//   // Calculate hue
+//   // No difference
+//   if (delta == 0)
+//     h = 0;
+//   // Red is max
+//   else if (cmax == r)
+//     h = ((g - b) / delta) % 6;
+//    // Green is max
+//   else if (cmax == g)
+//     h = (b - r) / delta + 2;
+//   // Blue is max
+//   else
+//     h = (r - g) / delta + 4;
+//     h = Math.round(h * 60);
  
-  // Make negative hues positive behind 360°
-  if (h < 0)
-    h += 360;
+//   // Make negative hues positive behind 360°
+//   if (h < 0)
+//     h += 360;
  
-  // Calculate lightness
-  l = (cmax + cmin) / 2;
+//   // Calculate lightness
+//   l = (cmax + cmin) / 2;
  
-  // Calculate saturation
-  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+//   // Calculate saturation
+//   s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
  
-  // Multiply l and s by 100
-  s = +(s * 100).toFixed(1);
-  l = +(l * 100).toFixed(1);
+//   // Multiply l and s by 100
+//   s = +(s * 100).toFixed(1);
+//   l = +(l * 100).toFixed(1);
  
-  return [h, s, l];
-}
+//   return [h, s, l];
+// }
 
-const hsl = rgbToHsl( bgColor[0], bgColor[1], bgColor[2] );
+// const hsl = rgbToHsl( bgColor[0], bgColor[1], bgColor[2] );
 
-window.addEventListener( 'scroll', () => {
-  let y = ( window.scrollY || window.pageYOffset ) / 100;
-  y = y < 0 ? 0 : y;  // Sarari でのエラスティック(マイナス方向への)スクロール対策
+// window.addEventListener( 'scroll', () => {
+//   let y = ( window.scrollY || window.pageYOffset ) / 100;
+//   y = y < 0 ? 0 : y;  // Sarari でのエラスティック(マイナス方向への)スクロール対策
   
-  // スクロール量を元に適度な lig 値を求める
-  const lig = hsl[2] + y * 10,
-         [ h, s, l ] = [ hsl[0], hsl[1], lig ];
+//   // スクロール量を元に適度な lig 値を求める
+//   const lig = hsl[2] + y * 10,
+//          [ h, s, l ] = [ hsl[0], hsl[1], lig ];
  
-  // 対象要素の background-color を HSL 形式で lig だけ変更
-  //container.style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`
+//   // 対象要素の background-color を HSL 形式で lig だけ変更
+//   //container.style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`
 
-  scene.background = new THREE.Color(`hsl(${h}, ${s}%, ${l}%)`);
- } );
+//   scene.background = new THREE.Color(`hsl(${h}, ${s}%, ${l}%)`);//これじゃダメ、、、
+//  } );
 
 
 const scene = new THREE.Scene();
-//scene.background = new THREE.Color(0xb8bebf);
+scene.background = new THREE.Color(0xb8bebf);
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.querySelector("#threeJS").appendChild( renderer.domElement );
 
-const light = new THREE.DirectionalLight(0xffffff);
+var light = new THREE.DirectionalLight(0xffffff);
 light.intensity = 2; // 光の強さを倍に
-// シーンに追加
-scene.add(light);
+
+directionalLight.shadow.camera.left = -10;
+    directionalLight.shadow.camera.right = 10;
+    directionalLight.shadow.camera.top = -10;
+    directionalLight.shadow.camera.bottom = 10;
+
 // ライトの位置を変更
-light.position.set(10, 10, 10);
+light.position.set(0, 1, 0).normalize();
+
+scene.add(light);
 
 const geometry = new THREE.BoxGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial( { color: 0x808080 } );
